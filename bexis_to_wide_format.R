995a5f18312a050c45ce6cd58647139bb9c7f139
noelle <noelle.schenk@orss.ch>
2023-03-23 12:20:49 +0100

    updated script

--- bexis_to_wide_format.R
+++ bexis_to_wide_format.R
@@ -27,31 +27,23 @@
 # - dataset 27087 with attachements from bexis
 #    - attachments : "synthesis_grassland_function_metadata_ID27087.csv" is used for the conversion.
 
-##
+## GET REQUIREMENTS ----
 # install required packages
 install.packages("data.table")
 install.packages("reshape2")
-
-
-##
-# load required packages
 library(data.table)
 library(reshape2)
-
-##
-# load datasets
-# path_to_bexis_datasets <- "" # fill in path to the BExIS dataset.
-path_to_bexis_datasets <- "~/IPS_SYNCFILES_27087_synthesis_grassland_functions/NEW_functions_dataset_needs_to_be_uploaded/"
+### load datasets
+path_to_bexis_datasets <- "~/Document/Project1/" # fill in path to the BExIS dataset.
+# metadata info
 additional_info <- fread(paste(path_to_bexis_datasets, "synthesis_grassland_function_metadata_ID27087.csv", sep = ""))
 additional_info <- additional_info[, .(ColumnName, AggregatedColumnName, codedYear)]
 setnames(additional_info, old = c("ColumnName", "codedYear"), new = c("variable", "Year"))
-#TODO : take out the following line, uncomment the last line
-original_synth_func <- fread("~/IPS_SYNCFILES_27087_synthesis_grassland_functions/NEW_functions_dataset_needs_to_be_uploaded/jan2022_raw_functions_dataset_bexisformat_long.csv")
-original_synth_func[, Soil_depth := NULL]
-original_synth_func[, Soil_C_concentration := NULL]
-# original_synth_func <- fread(paste(path_to_bexis_datasets, "27087_21_data.csv", sep = ""))
+# dataset
+original_synth_func <- fread(paste(path_to_bexis_datasets, "27087_25_data.csv", sep = ""))
 
-##
+
+## TRANSFORM ----
 # get function-year combinations as columns
 # make format even longer to obtain a column "Year" and a column "function"
 synth_func <- data.table(melt(original_synth_func, id.vars = c("Plot", "Plotn", "Explo", "Year")))
@@ -64,19 +56,24 @@ synth_func <- merge(synth_func, additional_info, by = c("variable", "Year"))
 synth_func <- synth_func[, .(AggregatedColumnName, Plot, Plotn, Explo, value)]
 synth_func <- data.table(dcast(synth_func, Plot + Plotn + Explo ~ AggregatedColumnName, value.var = "value"))
 
-##
-# quality control
+
+## QUALITY CONTROL ----
 # select some random functions from the old format and the new format and visually
 #    check if the values are still exactly the same (perfect correlation).
 # synth_func is the newly created dataset, original_synth_func is the read in dataset
 #   taking out "NM" from the original and "NA" from the wide format.
-plot(synth_func[!is.na(Total.pollinators), Total.pollinators], original_synth_func[!Total_pollinators %in% "NM", Total_pollinators])
-plot(synth_func[!is.na(Urease), Urease], original_synth_func[!Urease %in% "NM", Urease])
-plot(synth_func[!is.na(amoA_AOA.2016), amoA_AOA.2016], original_synth_func[Year == "2016",][!amoA_AOA %in% "NM", amoA_AOA])
-plot(synth_func$Groundwater.recharge2013, original_synth_func[Year == 2013, Groundwater_recharge])
-plot(synth_func[!is.na(Soil.C.stock), Soil.C.stock], original_synth_func[!Soil_C_stock %in% "NM", Soil_C_stock])
+plot(as.numeric(synth_func[!is.na(Total.pollinators), Total.pollinators]), 
+     as.numeric(original_synth_func[!Total_pollinators %in% c("NM", NA), Total_pollinators]))
+plot(as.numeric(synth_func[!is.na(Urease), Urease]),
+     as.numeric(original_synth_func[!Urease %in% c("NM", NA), Urease]))
+plot(as.numeric(synth_func[!is.na(amoA_AOA.2016), amoA_AOA.2016]),
+     as.numeric(original_synth_func[Year == "2016",][!amoA_AOA %in% c("NM", NA), amoA_AOA]))
+plot(synth_func$Groundwater.recharge2013, 
+     original_synth_func[Year == 2013, Groundwater_recharge])
+plot(synth_func[!is.na(Soil.C.stock), Soil.C.stock], 
+     original_synth_func[!Soil_C_stock %in% c(NA, "NM"), Soil_C_stock])
 # quality control successful?
 
-##
-# save reformatted file
-fwrite(synth_func, file = "bexis_to_wide_format_output", sep = ";")
+
+## SAVE NEW FILE ----
+fwrite(synth_func, file = "bexis_to_wide_format_output.csv", sep = ";")
