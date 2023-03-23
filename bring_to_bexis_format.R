328e0c1d997b744e1232d3d8494da67290750fb8
noelle <noelle.schenk@orss.ch>
2023-03-23 14:26:28 +0100

    Handling error in data format : for assembled years, we use 444444.0 to encode. Earlier, 4444.0 was used. Updating the dataset just adds or changes rows, but rows can not be taken out. Therefore, the entries for the year "4444.0" are all set to "NM" = not measured, such that they can be excluded by the researchers and no counfounding is possible between the year 4444.0 and 444444.0

--- bring_to_bexis_format.R
+++ bring_to_bexis_format.R
@@ -13,7 +13,7 @@ require(data.table)
 # helper dataset with names
 varkey <- fread("format_colnames_years_key.csv")
 # synthesisdataset <- fread("<path-to-synthesis-dataset>/jan2022_raw_functions_dataset.csv")
-# synthesisdataset <- data.table::copy(raw_functions) # if output from 2calc_raw_dataset.Rmd is still in environment
+synthesisdataset <- data.table::copy(raw_functions) # if output from 2calc_raw_dataset.Rmd is still in environment
 
 # convert numeric column to numeric
 num <- colnames(synthesisdataset)[!colnames(synthesisdataset) %in% c("Plot", "Plotn", "Explo")]
@@ -30,6 +30,14 @@ varkey$oldnames[which(!varkey$oldnames %in% unique(synthesisdataset$oldnames))] 
 
 synthesisdataset <- merge(synthesisdataset, varkey[, .(oldnames, short_varname, Year)], by = "oldnames")
 synthesisdataset[, oldnames := NULL]
+# Data type "double" allows up to 15 digits behind the comma. Round all values to 14 digits.
+synthesisdataset[, value := round(value, 7)]
+# AMOA_AOA and AOB have very large numbers --> round to 3 digits
+synthesisdataset[short_varname == "amoA_AOA", value := round(value, 3)]
+synthesisdataset[short_varname == "amoA_AOB", value := round(value, 3)]
+synthesisdataset[short_varname == "16S_NB", value := round(value, 3)]
+
+
 synthesisdataset[, value := as.character(value)]
 synthesisdataset <- data.table(dcast(synthesisdataset, Plot + Plotn + Explo + Year ~ short_varname, fill = "NM"))
 # NM = not measured --> distinguishes function-year combinations which were not measured from
@@ -54,10 +62,31 @@ ncol(ordered_synthesisdataset) == ncol(synthesisdataset)
 names(synthesisdataset)[!names(synthesisdataset) %in% names(ordered_synthesisdataset)] # no column missed
 synthesisdataset <- data.table::copy(ordered_synthesisdataset)
 rm(ordered_synthesisdataset)
+
+# final checks
+nrow(unique(synthesisdataset)) == nrow(synthesisdataset) # no duplicates
+length(unique(synthesisdataset$Year))
+length(unique(synthesisdataset$Year))
+nrow(synthesisdataset) == 150 * 13 # expected number of plots there
+
 fwrite(synthesisdataset, file = "march2023_raw_functions_dataset_bexisformat_long.csv", dec = ".", sep = ",", quote = F, na = "NA")
+
+# Dataset tuned for BExIS upload to 27087
 synthesisdataset[, Soil_depth := NULL]
 synthesisdataset[, Soil_C_concentration := NULL]
+# dealing with artefact : there are values for year "4444.0", which was removed.
+# No option to delete this year, therefore adding NA values in order to prevent people to use
+# this fake year. 
+# Note : assembled years are tagged with 444444.0, not with 4444.0
+artefact_year <- synthesisdataset[Year == "444444", ]
+artefact_year[, Year := 4444.0]
+artefact_functions_cols <- names(artefact_year)[!names(artefact_year) %in% c("Plot", "Plotn", "Explo", "Year")]
+artefact_year[, (artefact_functions_cols) := "NM"]
+# add to synthesisdataset
+synthesisdataset <- rbindlist(list(synthesisdataset, artefact_year))
+nrow(synthesisdataset) == 14 * 150
 fwrite(synthesisdataset, file = "27087_data_bexisformat.csv", dec = ".", sep = ",", quote = F, na = "NA")
+
 
 
 # the below lines are generating upload helpers. Since the transition to Bexis2, the lines were not tested
